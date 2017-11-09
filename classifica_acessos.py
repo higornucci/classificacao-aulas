5e8ef8cbc16de848d8265979bc51aedb6ecaf7ef
higor.nucci <higor.nucci@ufms.br>
2017-11-09 14:49:01 -0300

    divisão do conjuntos entre teste e treinp

--- classifica_acessos.py
+++ classifica_acessos.py
@@ -1,9 +1,24 @@
-from dados import carregar_acessos
+import numpy as np
 
+from dados import carregar_acessos
 X, Y = carregar_acessos()
+
+treino_dados = X[:90]
+treino_marcacoes = Y[:90]
+
+teste_dados = X[-9:]
+teste_marcacoes = Y[-9:]
+
 from sklearn.naive_bayes import MultinomialNB
 
 modelo = MultinomialNB()
-modelo.fit(X, Y)
+modelo.fit(treino_dados, treino_marcacoes)
 
-print(modelo.predict([[1, 0, 1], [0, 1, 0], [1, 0, 0]]))
+resultado = modelo.predict(teste_dados)
+diferencas = np.array(resultado).reshape(9, 1) - np.array(teste_marcacoes)
+acertos = [d for d in diferencas if d == 0]
+total_de_acertos = len(acertos)
+total_de_elementos = len(teste_dados)
+taxa_de_acertos = (total_de_acertos / total_de_elementos) * 100
+print(taxa_de_acertos)
+print(total_de_elementos)
