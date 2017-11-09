f4b3cedd4444de6eb0f6c2b7bc46737c1b6ef886
higor.nucci <higor.nucci@ufms.br>
2017-11-09 10:10:07 -0300

    predição do modelo de vendas com naive bayes

--- classifica_acessos.py
+++ classifica_acessos.py
@@ -1,4 +1,9 @@
 from dados import carregar_acessos
 
 X, Y = carregar_acessos()
-print(X, Y)
+from sklearn.naive_bayes import MultinomialNB
+
+modelo = MultinomialNB()
+modelo.fit(X, Y)
+
+print(modelo.predict([[1, 0, 1], [0, 1, 0], [1, 0, 0]]))
