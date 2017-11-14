b7f7705064414db4edb0dd58af07a3d975f0870f
higor.nucci <higor.nucci@ufms.br>
2017-11-14 15:09:15 -0300

    validate scores

--- classifica_buscas.py
+++ classifica_buscas.py
@@ -11,4 +11,33 @@ Ydummies_df = Y_df
 X = Xdummies_df.values
 Y = Ydummies_df.values
 
-print(X[567])
+porcentagem_treino = 0.9
+tamanho_de_treino = int(porcentagem_treino * len(Y))
+print(tamanho_de_treino)
+tamanho_de_teste = len(Y) - tamanho_de_treino
+treino_dados = X[:tamanho_de_treino]
+treino_marcacoes = Y[:tamanho_de_treino]
+
+teste_dados = X[-tamanho_de_teste:]
+teste_marcacoes = Y[-tamanho_de_teste:]
+
+# from sklearn.naive_bayes import MultinomialNB
+# modelo = MultinomialNB()
+
+from sklearn.ensemble import AdaBoostClassifier
+modelo = AdaBoostClassifier()
+
+modelo.fit(treino_dados, treino_marcacoes)
+
+resultado = modelo.predict(teste_dados)
+
+diferencas = resultado - teste_marcacoes
+
+acertos = [d for d in diferencas if d == 0]
+total_de_acertos = len(acertos)
+total_de_elementos = len(teste_dados)
+
+taxa_de_acerto = 100.0 * total_de_acertos / total_de_elementos
+
+print(taxa_de_acerto)
+print(total_de_elementos)
