94eb20a12b539333396b1af218756128a777b5ce
higor.nucci <higor.nucci@ufms.br>
2017-11-16 16:34:30 -0300

    validate scores

--- classifica_buscas.py
+++ classifica_buscas.py
@@ -5,19 +5,22 @@ df = pd.read_csv('buscas.csv')
 X_df = df[['home', 'busca', 'logado']]
 Y_df = df['comprou']
 
-Xdummies_df = pd.get_dummies(X_df)
+Xdummies_df = pd.get_dummies(X_df).astype(int)
 Ydummies_df = Y_df
 
 X = Xdummies_df.values
 Y = Ydummies_df.values
 
+acerto_de_um = sum(Y)
+acerto_de_zero = len(Y) - acerto_de_um
+taxa_de_acerto_base = 100.0 * max(acerto_de_um, acerto_de_zero) / len(Y)
+print("Taxa de acerto base: %.2f" % taxa_de_acerto_base)
+
 porcentagem_treino = 0.9
 tamanho_de_treino = int(porcentagem_treino * len(Y))
-print(tamanho_de_treino)
 tamanho_de_teste = len(Y) - tamanho_de_treino
 treino_dados = X[:tamanho_de_treino]
 treino_marcacoes = Y[:tamanho_de_treino]
-
 teste_dados = X[-tamanho_de_teste:]
 teste_marcacoes = Y[-tamanho_de_teste:]
 
@@ -39,5 +42,5 @@ total_de_elementos = len(teste_dados)
 
 taxa_de_acerto = 100.0 * total_de_acertos / total_de_elementos
 
-print(taxa_de_acerto)
+print("Taxa de acerto do algoritmo: %.2f" % taxa_de_acerto)
 print(total_de_elementos)
