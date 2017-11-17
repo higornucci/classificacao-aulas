8da32d720fbd5ae753bdf921f5fdc49224d0ae78
higor.nucci <higor.nucci@ufms.br>
2017-11-17 15:33:37 -0300

    usando k-fold

--- cliente/situacao_do_cliente.py
+++ cliente/situacao_do_cliente.py
@@ -1,5 +1,8 @@
 import pandas as pd
+import numpy as np
 from collections import Counter
+
+from sklearn.model_selection import cross_val_score
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.multiclass import OneVsRestClassifier
@@ -16,31 +19,20 @@ X = Xdummies_df.values
 Y = Ydummies_df.values
 
 porcentagem_de_treino = 0.8
-porcentagem_de_teste = 0.1
 
 tamanho_de_treino = int(porcentagem_de_treino * len(Y))
-tamanho_de_teste = int(porcentagem_de_teste * len(Y))
-tamanho_de_validacao = int(len(Y) - tamanho_de_treino - tamanho_de_teste)
 
 treino_dados = X[:tamanho_de_treino]
 treino_marcacoes = Y[:tamanho_de_treino]
 
-fim_de_treino = tamanho_de_treino + tamanho_de_teste
-
-teste_dados = X[tamanho_de_treino:fim_de_treino]
-teste_marcacoes = Y[tamanho_de_treino:fim_de_treino]
-
-validacao_dados = X[fim_de_treino:]
-validacao_marcacoes = Y[fim_de_treino:]
+validacao_dados = X[tamanho_de_treino:]
+validacao_marcacoes = Y[tamanho_de_treino:]
 
 
-def fit_and_predict(nome, modelo, treino_dados, treino_marcacoes, teste_dados, teste_marcacoes):
-    modelo.fit(treino_dados, treino_marcacoes)
-    resultado = modelo.predict(teste_dados)
-    acertos = resultado == teste_marcacoes
-    total_de_acertos = sum(acertos)
-    total_de_elementos = len(teste_dados)
-    taxa_de_acerto = 100.0 * total_de_acertos / total_de_elementos
+def fit_and_predict(nome, modelo, treino_dados, treino_marcacoes):
+    kfold = 4
+    scores = cross_val_score(modelo, treino_dados, treino_marcacoes, cv=kfold)
+    taxa_de_acerto = np.mean(scores)
     msg = "Taxa de acerto do algoritmo {0}: {1}".format(nome, taxa_de_acerto)
     print(msg)
     return taxa_de_acerto
@@ -59,22 +51,20 @@ def teste_real(modelo, validacao_dados, validacao_marcacoes):
 resultados = {}
 
 modeloOneVsRest = OneVsRestClassifier(LinearSVC())
-resultadoOneVsRest = fit_and_predict('OneVsRest', modeloOneVsRest, treino_dados, treino_marcacoes, teste_dados,
-                                     teste_marcacoes)
+resultadoOneVsRest = fit_and_predict('OneVsRest', modeloOneVsRest, treino_dados, treino_marcacoes)
 resultados[resultadoOneVsRest] = modeloOneVsRest
 
 modeloMultinomial = MultinomialNB()
-resultadoMultinomial = fit_and_predict("MultinomialNB", modeloMultinomial, treino_dados, treino_marcacoes, teste_dados,
-                                       teste_marcacoes)
+resultadoMultinomial = fit_and_predict("MultinomialNB", modeloMultinomial, treino_dados, treino_marcacoes)
 resultados[resultadoMultinomial] = modeloMultinomial
 
 modeloAdaBoost = AdaBoostClassifier()
-resultadoAdaBoost = fit_and_predict("AdaBoostClassifier", modeloAdaBoost, treino_dados, treino_marcacoes, teste_dados,
-                                    teste_marcacoes)
+resultadoAdaBoost = fit_and_predict("AdaBoostClassifier", modeloAdaBoost, treino_dados, treino_marcacoes)
 resultados[resultadoAdaBoost] = modeloAdaBoost
 maximo = max(resultados)
 vencedor = resultados[maximo]
 
+vencedor.fit(validacao_dados, validacao_marcacoes)
 teste_real(vencedor, validacao_dados, validacao_marcacoes)
 
 acerto_base = max(Counter(validacao_marcacoes).values())
