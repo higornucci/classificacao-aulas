c53ecf4469e3c63492b407aea05ffd54b143aa3b
Higor Nucci <higornucci@gmail.com>
2018-09-03 15:52:06 -0400

    Visualizando os dados

--- handson/housing.py
+++ handson/housing.py
@@ -1,16 +1,44 @@
+from __future__ import division, print_function, unicode_literals
+
 import os
 import tarfile
-import pandas as pd
-import hashlib
+import warnings
+
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 from six.moves import urllib
+from pandas.plotting import scatter_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedShuffleSplit
+
+warnings.filterwarnings(action="ignore", message="^internal gelsd")
 
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 2000)
 
+np.random.seed(42)
+
+plt.rcParams['axes.labelsize'] = 14
+plt.rcParams['xtick.labelsize'] = 12
+plt.rcParams['ytick.labelsize'] = 12
+
+PROJECT_ROOT_DIR = "."
+CHAPTER_ID = "end_to_end_project"
+IMAGES_PATH = os.path.join(PROJECT_ROOT_DIR, "images", CHAPTER_ID)
+
 DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
 HOUSING_PATH = "datasets/housing"
 HOUSING_URL = DOWNLOAD_ROOT + HOUSING_PATH + "/housing.tgz"
+
+
+def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
+    path = os.path.join(IMAGES_PATH, fig_id + "." + fig_extension)
+    print("Saving figure", fig_id)
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format=fig_extension, dpi=resolution)
 
 
 def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
@@ -28,24 +56,78 @@ def load_housing_data(housing_path=HOUSING_PATH):
     return pd.read_csv(csv_path)
 
 
-def test_set_check(identifier, test_ratio, hash):
-    return hash(np.int64(identifier)).digest()[-1] < 256 * test_ratio
-
-
-def split_train_test_by_id(data, test_ratio, id_column, hash=hashlib.md5):
-    ids = data[id_column]
-    in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio, hash))
-    return data.loc[~in_test_set], data.loc[in_test_set]
+def income_cat_proportions(data):
+    return data["income_cat"].value_counts() / len(data)
 
 
 housing = load_housing_data()
 
-housing_with_id = housing.reset_index()
-train_set, test_set = split_train_test_by_id(housing_with_id, 0.2, "index")
-
 print(housing.head(30))
 print(housing.info())
-
 print(housing["ocean_proximity"].value_counts())
-
 print(housing.describe())
+
+housing["income_cat"] = np.ceil(housing["median_income"] / 1.5)
+housing["income_cat"].where(housing["income_cat"] < 5, 5.0, inplace=True)
+
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+for train_index, test_index in split.split(housing, housing["income_cat"]):
+    strat_train_set = housing.loc[train_index]
+    strat_test_set = housing.loc[test_index]
+
+print(strat_test_set["income_cat"].value_counts() / len(strat_test_set))
+print(housing["income_cat"].value_counts() / len(housing))
+
+train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
+
+compare_props = pd.DataFrame({
+    "Overall": income_cat_proportions(housing),
+    "Stratified": income_cat_proportions(strat_test_set),
+    "Random": income_cat_proportions(test_set),
+}).sort_index()
+compare_props["Rand. %error"] = 100 * compare_props["Random"] / compare_props["Overall"] - 100
+compare_props["Strat. %error"] = 100 * compare_props["Stratified"] / compare_props["Overall"] - 100
+print(compare_props)
+
+for set_ in (strat_train_set, strat_test_set):
+    set_.drop(["income_cat"], axis=1, inplace=True)
+
+# visualizando os dados
+
+housing = strat_train_set.copy()
+
+# california_img = mpimg.imread(PROJECT_ROOT_DIR + '/images/end_to_end_project/california.png')
+# ax = housing.plot(kind="scatter", x="longitude", y="latitude", figsize=(10,7),
+#                        s=housing['population']/100, label="Population",
+#                        c="median_house_value", cmap=plt.get_cmap("jet"),
+#                        colorbar=False, alpha=0.4,
+#                       )
+# plt.imshow(california_img, extent=[-124.55, -113.80, 32.45, 42.05], alpha=0.5,
+#            cmap=plt.get_cmap("jet"))
+# plt.ylabel("Latitude", fontsize=14)
+# plt.xlabel("Longitude", fontsize=14)
+#
+# prices = housing["median_house_value"]
+# tick_values = np.linspace(prices.min(), prices.max(), 11)
+# cbar = plt.colorbar()
+# cbar.ax.set_yticklabels(["$%dk"%(round(v/1000)) for v in tick_values], fontsize=14)
+# cbar.set_label('Median House Value', fontsize=16)
+#
+# plt.legend(fontsize=16)
+# save_fig("california_housing_prices_plot")
+# plt.show()
+
+corr_matrix = housing.corr()
+print(corr_matrix["median_house_value"].sort_values(ascending=False))
+
+attributes = ["median_house_value", "median_income", "total_rooms",
+              "housing_median_age"]
+scatter_matrix(housing[attributes], figsize=(12, 8))
+save_fig("scatter_matrix_plot")
+
+housing["rooms_per_household"] = housing["total_rooms"]/housing["households"]
+housing["bedrooms_per_room"] = housing["total_bedrooms"]/housing["total_rooms"]
+housing["population_per_household"]=housing["population"]/housing["households"]
+
+corr_matrix = housing.corr()
+corr_matrix["median_house_value"].sort_values(ascending=False)
