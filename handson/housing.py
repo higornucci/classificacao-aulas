18d96bd979f3854c23ed15a212c96e1ac4533f51
Higor Nucci <higornucci@gmail.com>
2018-09-03 13:51:56 -0400

    Quebrando o dataset em train e test para validação

---
+++ handson/housing.py
@@ -0,0 +1,51 @@
+import os
+import tarfile
+import pandas as pd
+import hashlib
+import numpy as np
+from six.moves import urllib
+
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 2000)
+
+DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
+HOUSING_PATH = "datasets/housing"
+HOUSING_URL = DOWNLOAD_ROOT + HOUSING_PATH + "/housing.tgz"
+
+
+def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
+    if not os.path.isdir(housing_path):
+        os.makedirs(housing_path)
+    tgz_path = os.path.join(housing_path, "housing.tgz")
+    urllib.request.urlretrieve(housing_url, tgz_path)
+    housing_tgz = tarfile.open(tgz_path)
+    housing_tgz.extractall(path=housing_path)
+    housing_tgz.close()
+
+
+def load_housing_data(housing_path=HOUSING_PATH):
+    csv_path = os.path.join(housing_path, "housing.csv")
+    return pd.read_csv(csv_path)
+
+
+def test_set_check(identifier, test_ratio, hash):
+    return hash(np.int64(identifier)).digest()[-1] < 256 * test_ratio
+
+
+def split_train_test_by_id(data, test_ratio, id_column, hash=hashlib.md5):
+    ids = data[id_column]
+    in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio, hash))
+    return data.loc[~in_test_set], data.loc[in_test_set]
+
+
+housing = load_housing_data()
+
+housing_with_id = housing.reset_index()
+train_set, test_set = split_train_test_by_id(housing_with_id, 0.2, "index")
+
+print(housing.head(30))
+print(housing.info())
+
+print(housing["ocean_proximity"].value_counts())
+
+print(housing.describe())
