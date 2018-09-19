12ca84d951e6d8c6f2852d0f6bbf2b3de9ec8ce3
Higor Nucci <higornucci@gmail.com>
2018-09-19 16:31:27 -0400

    Aplicando algoritmos de ML

--- dissertacao/3_visualizar_dados.py
+++ dissertacao/3_visualizar_dados.py
@@ -8,22 +8,20 @@ sns.set_style('whitegrid')
 pd.set_option('display.max_columns', None)  # display all columns
 
 dados_completo = pd.read_csv('../input/DadosCompletoTransformado.csv', encoding='utf-8', delimiter='\t')
-dados_completo.set_index('estabelecimento_identificador', inplace=True)
+dados_completo.set_index('index', inplace=True)
 
 # Transformando tipos de dados de colunas numéricas
 dados_completo['identificador_lote_situacao_lote'] = dados_completo['identificador_lote_situacao_lote'].astype(
     'category')
-dados_completo['eh_novilho_precoce'] = dados_completo['eh_novilho_precoce'].astype('int32')
-dados_completo['tipificacao'] = dados_completo['tipificacao'].astype('int32')
+dados_completo['tipificacao'] = dados_completo['tipificacao'].astype('category')
 dados_completo['maturidade'] = dados_completo['maturidade'].astype('int32')
 dados_completo['acabamento'] = dados_completo['acabamento'].astype('int32')
+dados_completo['rispoa'] = dados_completo['rispoa'].astype('int32')
+dados_completo['peso'] = dados_completo['peso'].astype('float32')
 dados_completo['aprovacao_carcaca_sif'] = dados_completo['aprovacao_carcaca_sif'].astype('int32')
-dados_completo['estabelecimento_municipio'] = dados_completo['estabelecimento_municipio'].astype('category')
-dados_completo['estabelecimento_uf'] = dados_completo['estabelecimento_uf'].astype('category')
-dados_completo['ṕossui_outros_incentivos'] = dados_completo['ṕossui_outros_incentivos'].astype('int32')
-dados_completo['produtor_situacao'] = dados_completo['produtor_situacao'].astype('int32')
-dados_completo['pratica_recuperacao_pastagem_outra_pratica'] = dados_completo[
-    'pratica_recuperacao_pastagem_outra_pratica'].astype('category')
+dados_completo['questionario_classificacao_estabelecimento_rural'] = dados_completo[
+    'questionario_classificacao_estabelecimento_rural'].astype('int32')
+dados_completo['possui_outros_incentivos'] = dados_completo['possui_outros_incentivos'].astype('int32')
 dados_completo['fabrica_racao'] = dados_completo['fabrica_racao'].astype('int32')
 dados_completo['area_total_destinada_confinamento'] = dados_completo['area_total_destinada_confinamento'].astype(
     'int32')
@@ -39,41 +37,18 @@ dados_completo['lita_trace'] = dados_completo['lita_trace'].astype('int32')
 dados_completo['apresenta_atestado_programas_controle_qualidade'] = dados_completo[
     'apresenta_atestado_programas_controle_qualidade'].astype('int32')
 dados_completo['envolvido_em_organizacao'] = dados_completo['envolvido_em_organizacao'].astype('int32')
-dados_completo['confinamento_alto_concentrado'] = dados_completo['confinamento_alto_concentrado'].astype('int32')
-dados_completo['confinamento_alto_concentrado_volumoso'] = dados_completo[
-    'confinamento_alto_concentrado_volumoso'].astype('int32')
-dados_completo['confinamento_concentrado_volumoso'] = dados_completo['confinamento_concentrado_volumoso'].astype(
-    'int32')
-dados_completo['confinamento_grao_inteiro'] = dados_completo['confinamento_grao_inteiro'].astype('int32')
-dados_completo['confinamento_racao_consumo_igual_superior_0_8_porcento_peso_vivo'] = dados_completo[
-    'confinamento_racao_consumo_igual_superior_0_8_porcento_peso_vivo'].astype('int32')
-dados_completo['confinamento_racao_consumo_inferior_0_8_porcento_peso_vivo'] = dados_completo[
-    'confinamento_racao_consumo_inferior_0_8_porcento_peso_vivo'].astype('int32')
-dados_completo['semi_confinamento_racao_consumo_igual_superior_0_8_porcento_peso_vivo'] = dados_completo[
-    'semi_confinamento_racao_consumo_igual_superior_0_8_porcento_peso_vivo'].astype('int32')
-dados_completo['semi_confinamento_racao_consumo_inferior_0_8_porcento_peso_vivo'] = dados_completo[
-    'semi_confinamento_racao_consumo_inferior_0_8_porcento_peso_vivo'].astype('int32')
-dados_completo['suplementacao_a_campo_creep_feeding'] = dados_completo['suplementacao_a_campo_creep_feeding'].astype(
-    'int32')
-dados_completo['suplementacao_a_campo_silagem_ou_feno'] = dados_completo[
-    'suplementacao_a_campo_silagem_ou_feno'].astype('int32')
-dados_completo['suplementacao_a_campo_proteico'] = dados_completo['suplementacao_a_campo_proteico'].astype('int32')
-dados_completo['suplementacao_a_campo_proteico_energetico'] = dados_completo[
-    'suplementacao_a_campo_proteico_energetico'].astype('int32')
-dados_completo['suplementacao_a_campo_sal_mineral'] = dados_completo['suplementacao_a_campo_sal_mineral'].astype(
-    'int32')
-dados_completo['suplementacao_a_campo_sal_mineral_ureia'] = dados_completo[
-    'suplementacao_a_campo_sal_mineral_ureia'].astype('int32')
+dados_completo['confinamento'] = dados_completo['confinamento'].astype('int32')
+dados_completo['semi_confinamento'] = dados_completo['semi_confinamento'].astype('int32')
+dados_completo['suplementacao'] = dados_completo['suplementacao'].astype('int32')
 dados_completo['fertirrigacao'] = dados_completo['fertirrigacao'].astype('int32')
 dados_completo['ifp'] = dados_completo['ifp'].astype('int32')
 dados_completo['ilp'] = dados_completo['ilp'].astype('int32')
 dados_completo['ilpf'] = dados_completo['ilpf'].astype('int32')
-dados_completo['nenhum'] = dados_completo['nenhum'].astype('int32')
-dados_completo['data_abate'] = pd.to_datetime(dados_completo['data_abate'], dayfirst=True)
+dados_completo['latitude'] = dados_completo['latitude'].astype('float64')
+dados_completo['longitude'] = dados_completo['longitude'].astype('float64')
 
 print(dados_completo.shape)
 print(dados_completo.describe(include=['number']))
-print(dados_completo.describe(include=['datetime']))
 print(dados_completo.describe(include=['object', 'category']))
 
 train_set, test_set = train_test_split(dados_completo, test_size=0.2, random_state=42)
