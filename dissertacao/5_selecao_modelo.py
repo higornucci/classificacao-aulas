b18168e6dd2e7f572279ef92f7a9a9efe7489f70
Higor Nucci <higornucci@gmail.com>
2018-09-24 07:21:19 -0400

    Usando gridsearch

--- dissertacao/5_selecao_modelo.py
+++ dissertacao/5_selecao_modelo.py
@@ -1,7 +1,7 @@
 import warnings
 import time
 import pandas as pd
-from sklearn.model_selection import train_test_split, cross_val_score, KFold
+from sklearn.model_selection import train_test_split, cross_val_score, KFold, GridSearchCV
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
@@ -13,36 +13,78 @@ dados_completo = pd.read_csv('../input/DadosCompletoTransformadoML.csv', encodin
 dados_completo.set_index('index', inplace=True)
 
 conjunto_treinamento, conjunto_teste = train_test_split(dados_completo, test_size=0.2, random_state=42)
-print(conjunto_treinamento.head(10))
+conjunto_treinamento = conjunto_treinamento[:160000]
+conjunto_teste = conjunto_teste[-40000:]
 
-X_treino, X_teste, Y_treino, Y_teste = conjunto_treinamento.drop('acabamento', axis=1), conjunto_teste.drop('acabamento', axis=1), conjunto_treinamento['acabamento'], conjunto_teste['acabamento']
+X_treino, X_teste, Y_treino, Y_teste = conjunto_treinamento.drop('acabamento', axis=1), conjunto_teste.drop(
+    'acabamento', axis=1), conjunto_treinamento['acabamento'], conjunto_teste['acabamento']
 print('X Treino:', X_treino.head(10))
 print('Y Treino:', Y_treino.head(10))
 print('X Teste:', X_teste.head(10))
 print('Y Teste:', Y_teste.head(10))
+Y_teste.to_csv("y_teste.csv", encoding='utf-8', index=False)
 
 seed = 7
-num_folds = 3
-processors = 1
+num_folds = 5
+processors = 3
 scoring = 'accuracy'
 kfold = KFold(n_splits=num_folds, random_state=seed)
 
 # preparando alguns modelos
-modelos = [('SVM', SVC(decision_function_shape='ovo')),
-           ('K-NN', KNeighborsClassifier(n_neighbors=5)),
-           ('DTC', DecisionTreeClassifier(criterion='entropy', max_depth=3, random_state=0)),
-           ('NB', MultinomialNB())]
+modelos_base = [('SVM', SVC()),
+                ('K-NN', KNeighborsClassifier()),  # n_jobs=-1 roda com o mesmo número de cores
+                ('DTC', DecisionTreeClassifier()),
+                ('NB', MultinomialNB())]
 
-# Validar cada um dos modelos
-for nome, modelo in modelos:
+
+def rodar_algoritmos():
+    global inicio, preds, final, grid_search
     inicio = time.time()
-    cv_resultados = cross_val_score(modelo, X_treino, Y_treino, cv=kfold, scoring=scoring)
+    grid_search = GridSearchCV(modelo, escolher_parametros(), cv=kfold, scoring=scoring, n_jobs=-1)
+    grid_search.fit(X_treino, X_teste)
+    cv_resultados = cross_val_score(grid_search.best_estimator_, X_treino, Y_treino, cv=kfold, scoring=scoring)
     print("{0}: ({1:.4f}) +/- ({2:.3f})".format(nome, cv_resultados.mean(), cv_resultados.std()))
     modelo.fit(X_treino, Y_treino)
     preds = modelo.predict(X_teste)
     final = time.time()
+
+
+def escolher_parametros():
+    if nome == 'K-NN':
+        return [
+            {'n_neighbors': [1, 5, 10, 20, 50],
+             'weights': ['uniform', 'distance']}
+        ]
+    elif nome == 'SVM':
+        return [
+            {'C': [.01, .1, 1, 10, 100],
+             'gamma': [.0001, .001, .01, .1],
+             'kernel': ['rbf', 'poly']}
+        ]
+    elif nome == 'DTC':
+        return [
+            {'max_features': [1, 5, 10, 20, 40],
+             'max_depth': [1, 5, 10, 15, 30],
+             'class_weight': [None, 'balanced']}
+        ]
+    elif nome == 'NB':
+        return [
+            {'alpha': [0, .0001, .001, .01, .1, .5, 1, 2, 10]}
+        ]
+    return None
+
+
+def imprimir_resultados():
     resultado = pd.DataFrame()
     resultado["id"] = X_teste.index
     resultado["item.acabamento"] = preds
     resultado.to_csv("resultado_" + nome + ".csv", encoding='utf-8', index=False)
-    print('Tempo de execução do ' + nome + ':', final - inicio)
+    print('Tempo de execução do ' + nome + ': {0:.4f} segundos'.format(final - inicio))
+    feature_importances = grid_search.best_estimator_.feature_importances_
+    print('Importância das características:', feature_importances)
+
+
+# Validar cada um dos modelos
+for nome, modelo in modelos_base:
+    rodar_algoritmos()
+    imprimir_resultados()
