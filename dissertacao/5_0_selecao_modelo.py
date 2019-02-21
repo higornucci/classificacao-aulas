e6a2de8f77ba0242bd625d1b8591fe9ad30e9c2f
Higor Nucci <higornucci@gmail.com>
2019-02-21 15:27:23 -0400

    Gridsearch cv com cross validation

--- dissertacao/5_0_selecao_modelo.py
+++ dissertacao/5_0_selecao_modelo.py
@@ -1,3 +1,4 @@
+import itertools
 import warnings
 import time
 import multiprocessing
@@ -17,7 +18,7 @@ from sklearn.model_selection import cross_val_score, cross_val_predict, GridSear
     StratifiedShuffleSplit, RandomizedSearchCV
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score
+from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score, classification_report
 from sklearn.svm import SVC
 from sklearn import tree
 from sklearn.feature_selection import RFE
@@ -25,89 +26,28 @@ from sklearn.linear_model import LogisticRegression
 
 warnings.filterwarnings('ignore')
 
-# 1 Iris Setosa, 2 Iris Versicolour, 3 Iris Virginica
-# dados_completo = pd.read_csv('../input/iris.csv', encoding='utf-8', delimiter=',')
 dados_completo = pd.read_csv('../input/DadosCompletoTransformadoML.csv', encoding='utf-8', delimiter='\t')
 print(dados_completo.head())
 
 random_state = 42
-n_jobs = multiprocessing.cpu_count() - 1
-
-
-def mostrar_quantidade_por_classe(df, classe):
-    print(df.loc[df['acabamento'] == classe].info())
-
-
-def buscar_quantidades_iguais(quantidade, classe):
-    classe = dados_completo.loc[dados_completo['acabamento'] == classe]
-    return classe.sample(quantidade, random_state=random_state)
-
-
-def mostrar_correlacao(dados, classe):
-    matriz_correlacao = dados.corr()
-    print('Correlaçao com ' + classe + '\n', matriz_correlacao[classe].sort_values(ascending=False))
-
-    colunas = ['C', 'F', 'M', 'mat', 'peso', '% class', 'out_inc', 'fab_rac', 'area_conf', 'area_man_80_cob',
-               'area_man_20_er', 'id_ind', 'sisbov', 'cont_past', 'lita_trace', 'atest_prog_quali', 'envolvido_org',
-               'confi', 'semi_confi', 'suple', 'ferti', 'ifp', 'ilp', 'ilpf', 'lat', 'lon', 'prec', 'acab']
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    cax = ax.matshow(matriz_correlacao, vmin=-1, vmax=1)
-    fig.colorbar(cax)
-    ticks = np.arange(0, 28, 1)
-    ax.set_xticks(ticks)
-    ax.set_yticks(ticks)
-    ax.set_xticklabels(colunas)
-    ax.set_yticklabels(colunas)
-    plt.xticks(rotation=90)
-    plt.savefig('corr.svg')
-    plt.show()
-
-
-# mostrar_correlacao(dados_completo, 'acabamento')
-
-# classe_1 = buscar_quantidades_iguais(250, 1)
-# classe_2 = buscar_quantidades_iguais(350, 2)
-# classe_3 = buscar_quantidades_iguais(350, 3)
-# classe_4 = buscar_quantidades_iguais(350, 4)
-# classe_5 = buscar_quantidades_iguais(198, 5)
-# frames = [classe_1, classe_2, classe_3, classe_4, classe_5]
-# dados_qtde_iguais = pd.concat(frames)
+n_jobs = 2
 
 conjunto_treinamento = pd.DataFrame()
 conjunto_teste = pd.DataFrame()
-split = StratifiedShuffleSplit(n_splits=1, test_size=0.8, random_state=random_state)
-for trainamento_index, teste_index in split.split(dados_completo, dados_completo['acabamento']):
-    conjunto_treinamento = dados_completo.loc[trainamento_index]
-    conjunto_teste = dados_completo.loc[teste_index]
 
-# balanceador = ClusterCentroids(random_state=random_state)
-# balanceador = RandomUnderSampler(random_state=random_state)
-# balanceador = NearMiss(version=3)
-# balanceador = AllKNN(allow_minority=True)
-# balanceador = NeighbourhoodCleaningRule(n_jobs=n_jobs, sampling_strategy='auto')
-# balanceador = EditedNearestNeighbours(n_jobs=n_jobs, sampling_strategy='auto')
+# classes_balancear = list([2, 3])
+# print('Classes para balancear', classes_balancear)
+# balanceador = EditedNearestNeighbours(n_jobs=n_jobs, kind_sel='all',
+#                                       sampling_strategy=classes_balancear, n_neighbors=3)
+balanceador = SMOTEENN()
+# balanceador = SMOTE(n_jobs=n_jobs)
 
-# balanceador = SMOTE()
-# balanceador = ADASYN()
-# balanceador = RandomOverSampler()
+X_treino, Y_treino = conjunto_treinamento.drop('carcass_fatness_degree', axis=1), \
+                     conjunto_treinamento['carcass_fatness_degree']
 
-balanceador = SMOTEENN(random_state=random_state, sampling_strategy='auto')
-# X_treino, Y_treino = balanceador.fit_resample(
-#     conjunto_treinamento.drop('acabamento', axis=1),
-#     conjunto_treinamento['acabamento'])
-# print(sorted(Counter(Y_treino).items()))
-X_treino, Y_treino = conjunto_treinamento.drop('acabamento', axis=1), conjunto_treinamento['acabamento']
+X_teste, Y_teste = conjunto_teste.drop('carcass_fatness_degree', axis=1), \
+                   conjunto_teste['carcass_fatness_degree']
 
-X_teste, Y_teste = conjunto_teste.drop('acabamento', axis=1), conjunto_teste['acabamento']
-
-print('X Treino:', X_treino.info())
-# print('X Teste:', X_teste.info())
-mostrar_quantidade_por_classe(conjunto_treinamento, 1)
-mostrar_quantidade_por_classe(conjunto_treinamento, 2)
-mostrar_quantidade_por_classe(conjunto_treinamento, 3)
-mostrar_quantidade_por_classe(conjunto_treinamento, 4)
-mostrar_quantidade_por_classe(conjunto_treinamento, 5)
 resultado = pd.DataFrame()
 resultado["id"] = Y_teste.index
 resultado["item.classe"] = Y_teste.values
@@ -132,29 +72,64 @@ scoring = 'accuracy'
 kfold = StratifiedKFold(n_splits=num_folds, random_state=random_state)
 
 # preparando alguns modelos
-modelos_base = [('NB', MultinomialNB()),
-                ('DTC', tree.DecisionTreeClassifier()),
-                # ('RF', RandomForestClassifier(random_state=random_state)),
-                ('K-NN', KNeighborsClassifier()),  # n_jobs=-1 roda com o mesmo número de cores
-                # ('SVM', SVC())
-                ]
+modelos_base = [
+    ('MNB', MultinomialNB()),
+    ('RFC', RandomForestClassifier(random_state=random_state, oob_score=True)),
+    ('K-NN', KNeighborsClassifier()),  # n_jobs=-1 roda com o mesmo número de cores
+    ('SVM', SVC(gamma='scale'))
+]
 
 
-def gerar_matriz_confusao(modelo):
-    average = None
-    # y_train_pred = cross_val_predict(modelo, X_treino, Y_treino, cv=kfold)
-    X_treino_balanceado, Y_treino_balanceado = balanceador.fit_resample(X_treino, Y_treino)
-    modelo.fit(X_treino_balanceado, Y_treino_balanceado)
-    y_train_pred = modelo.predict(X_teste)
-    matriz_confusao = confusion_matrix(Y_teste, y_train_pred)
-    print('Matriz de Confusão')
+def plot_confusion_matrix(cm, nome, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.gray):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.figure()
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        np.set_printoptions(precision=2)
+        nome_arquivo = 'matriz_confusao_normalizada_' + nome + '.png'
+    else:
+        nome_arquivo = 'matriz_confusao_' + nome + '.png'
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="black" if cm[i, j] > thresh else "white")
+
+    plt.ylabel('Actual class')
+    plt.xlabel('Predicted class')
+    plt.grid('off')
+    plt.tight_layout()
+    plt.savefig('figuras/' + nome_arquivo)
+
+
+def gerar_matriz_confusao(modelo, nome, tipo, X_treino, Y_treino):
+    modelo.fit(X_treino, Y_treino.values.ravel())
+    y_pred = modelo.predict(X_teste)
+    matriz_confusao = confusion_matrix(Y_teste, y_pred)
+    print('Matriz de Confusão ' + tipo)
     print(matriz_confusao)
-    print(classification_report_imbalanced(Y_teste, y_train_pred))
+    plot_confusion_matrix(matriz_confusao, nome, [1, 2, 3, 4, 5], True,
+                          title='Confusion matrix ' + nome + ', normalized')
+    plot_confusion_matrix(matriz_confusao, nome, [1, 2, 3, 4, 5], False, title='Confusion matrix ' + nome)
+    print(classification_report(y_true=Y_teste, y_pred=y_pred, digits=4))
 
 
 def rodar_algoritmos():
-    global preds
-    inicio = time.time()
     pipeline = Pipeline([('bal', balanceador),
                          ('clf', modelo)])
     grid_search = GridSearchCV(pipeline, escolher_parametros(), cv=kfold, n_jobs=n_jobs)
@@ -162,23 +137,10 @@ def rodar_algoritmos():
     melhor_modelo = grid_search.best_estimator_
     cv_resultados = cross_val_score(melhor_modelo, X_treino, Y_treino, cv=kfold, scoring=scoring, n_jobs=n_jobs)
 
-    # mostrar_features_mais_importantes(melhor_modelo)
-
     print('Melhores parametros ' + nome + ' :', melhor_modelo)
     print('Validação cruzada ' + nome + ' :', cv_resultados)
     print("{0}: ({1:.4f}) +/- ({2:.3f})".format(nome, cv_resultados.mean(), cv_resultados.std()))
-    final = time.time()
-    print('Tempo de execução do ' + nome + ': {0:.4f} segundos'.format(final - inicio))
-    gerar_matriz_confusao(BaggingClassifier(melhor_modelo))
-
-
-def mostrar_features_mais_importantes(melhor_modelo):
-    if nome == 'RF':
-        print('Características mais importantes RF :')
-        feature_importances = pd.DataFrame(melhor_modelo.feature_importances_,
-                                           index=X_treino.columns,
-                                           columns=['importance']).sort_values('importance', ascending=False)
-        print(feature_importances)
+    gerar_matriz_confusao(melhor_modelo)
 
 
 def escolher_parametros():
@@ -197,24 +159,11 @@ def escolher_parametros():
                  # {'kernel': ['linear'],
                  #              'C': [0.001, 0.10, 0.1, 10, 25, 50, 100, 1000]
                  }]
-    elif nome == 'DTC':
-        return [{'clf__max_features': range(20, 29, 2),
-                 'clf__max_depth': [1, 10, 15, 16, 17],
-                 'clf__min_samples_split': range(10, 100, 5),
-                 'clf__min_samples_leaf': range(1, 30, 2),
-                 'clf__class_weight': [None, 'balanced']
-                 # },
-                 # {'max_features': 20,
-                 #  'max_depth': 13,
-                 #  'min_samples_split': 7,
-                 #  'min_samples_leaf': 17,
-                 #  # 'class_weight': [None, 'balanced']
-                 }]
-    elif nome == 'NB':
+    elif nome == 'MNB':
         return [{'clf__alpha': range(1, 10, 1),
                  'clf__fit_prior': [True, False],
                  'clf__class_prior': [None, [1, 2, 3, 4, 5]]}]
-    elif nome == 'RF':
+    elif nome == 'RFC':
         return [{'clf__n_estimators': range(10, 300, 50),
                  'clf__max_features': range(1, 27, 2),
                  'clf__max_depth': range(1, 10, 1),
@@ -225,13 +174,5 @@ def escolher_parametros():
     return None
 
 
-def imprimir_resultados():
-    resultado = pd.DataFrame()
-    resultado["id"] = X_teste.index
-    resultado["item.acabamento"] = preds
-    resultado.to_csv("resultado_" + nome + ".csv", encoding='utf-8', index=False)
-
-
 for nome, modelo in modelos_base:
     rodar_algoritmos()
-    # imprimir_resultados()
